f810a92b6407ba16ec7c148f3752b31d5a407c1c
Esmayk Tillesse <esmaykalves@gmail.com>
2024-09-10 23:37:13 -0300

    add config bd

--- app/main.py
+++ app/main.py
@@ -6,9 +6,11 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from flask import Flask
 from controllers.usuario_controller import endpoint
 from services.scheduler_service import start_scheduler_email
+from configs.config import Config
 
 app = Flask(__name__)
 app.register_blueprint(endpoint)
+app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
 
 if __name__ == "__main__":
     start_scheduler_email()
